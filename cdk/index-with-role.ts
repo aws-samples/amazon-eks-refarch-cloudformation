b866e71f2487bc43dd49b6dd12b16ea854c48229
Pahud <pahudnet@gmail.com>
2019-08-08 23:54:04 +0800

    format

--- cdk/index-with-role.ts
+++ cdk/index-with-role.ts
@@ -7,8 +7,8 @@ import lambda = require('@aws-cdk/aws-lambda');
 
 const app = new cdk.App();
 
-const env = { 
-  region:  app.node.tryGetContext("region")  || process.env.CDK_INTEG_REGION  || process.env.CDK_DEFAULT_REGION,
+const env = {
+  region: app.node.tryGetContext("region") || process.env.CDK_INTEG_REGION || process.env.CDK_DEFAULT_REGION,
 }
 
 const clusterName = app.node.tryGetContext("clusterName") || 'eksdemo-cdk'
@@ -24,25 +24,25 @@ class NetworkStack extends cdk.Stack {
     // each public subnet will privision individual NAT gateway with EIP attached.
     // This is how CDK provision VPC resource for you by default.
     // const vpc = new ec2.Vpc(this, 'cdk-EKS-vpc');
-    
+
     // VPC with only single NAT gateway in one of the public subnets
     this.vpc = new ec2.Vpc(this, 'cdk-EKS-vpc', {
       cidr: '10.0.0.0/16',
       natGateways: 1,
-      natGatewaySubnets: {subnetName: 'Public'},
+      natGatewaySubnets: { subnetName: 'Public' },
       subnetConfiguration: [
         {
           cidrMask: 22,
           name: 'Public',
-          subnetType: ec2.SubnetType.PUBLIC, 
+          subnetType: ec2.SubnetType.PUBLIC,
         },
         {
           cidrMask: 22,
           name: 'Private',
-          subnetType: ec2.SubnetType.PRIVATE, 
+          subnetType: ec2.SubnetType.PRIVATE,
         },
       ],
-    });    
+    });
   }
 }
 
@@ -51,68 +51,68 @@ interface EKSCdkStackProps extends cdk.StackProps {
 }
 
 class EKSCdkStack extends cdk.Stack {
-    constructor(scope: cdk.App, id: string, props: EKSCdkStackProps) {
-        super(scope, id, props);  
-        
-        // EKS Cluster
-        const eksCluster = new eks.Cluster(this, 'eksdemo-cdk', { 
-            vpc: props.vpc, 
-            clusterName: clusterName, 
-            version: this.node.tryGetContext('clusterVersion'),
-        });
-            
-        // NodeGroup
-        const asg = eksCluster.addCapacity('Nodes', {
-          instanceType: new ec2.InstanceType('t3.large'),
-          desiredCapacity: 2, 
-        });
-        
-        // aws-lambda-layer-kubectl
-        const lambdaLayerKubectl = new sam.CfnApplication(this, 'lambdaLayerKubectl', {
-          location: {
-            applicationId: 'arn:aws:serverlessrepo:us-east-1:903779448426:applications/lambda-layer-kubectl',
-            semanticVersion: '1.13.7',
-          },
-          parameters: {
-            LayerName: 'lambda-layer-kubectl'
-          }
-        })             
-        
+  constructor(scope: cdk.App, id: string, props: EKSCdkStackProps) {
+    super(scope, id, props);
 
-        // cfn custom resource to handle aws-auth-cm update
-        const eksAuthHook = new sam.CfnApplication(this, 'sam', {
-          location: {
-            applicationId: 'arn:aws:serverlessrepo:us-east-1:903779448426:applications/eks-auth-update-hook',
-            semanticVersion: '1.0.0',
-          },
-          parameters: {
-            ClusterName: 'eksdemo-cdk',
-            // LambdaRoleArn: 'arn:aws:iam::903779448426:role/AmazonEKSAdminRole',
-            LambdaRoleArn: lambdaRoleArn,
-            LambdaLayerKubectlArn: lambdaLayerKubectl.getAtt('Outputs.LayerVersionArn').toString(),
-            NodeInstanceRoleArn: asg.role.roleArn,
-            FunctionName: 'eks-auth-hook'
-          }
-        })     
+    // EKS Cluster
+    const eksCluster = new eks.Cluster(this, 'eksdemo-cdk', {
+      vpc: props.vpc,
+      clusterName: clusterName,
+      version: this.node.tryGetContext('clusterVersion'),
+    });
 
-        const customResourceAuthHook = new cfn.CustomResource(this, 'CustomResource', {
-          provider: cfn.CustomResourceProvider.lambda(lambda.Function.fromFunctionArn(this, 'SamFunc', eksAuthHook.ref)),
-          properties: {
-            ServiceToken: eksAuthHook.getAtt('Outputs.LambdaFuncArn').toString()
-          }
-        });
+    // NodeGroup
+    const asg = eksCluster.addCapacity('Nodes', {
+      instanceType: new ec2.InstanceType('t3.large'),
+      desiredCapacity: 2,
+    });
 
-        customResourceAuthHook.node.addDependency(eksCluster)
+    // aws-lambda-layer-kubectl
+    const lambdaLayerKubectl = new sam.CfnApplication(this, 'lambdaLayerKubectl', {
+      location: {
+        applicationId: 'arn:aws:serverlessrepo:us-east-1:903779448426:applications/lambda-layer-kubectl',
+        semanticVersion: '1.13.7',
+      },
+      parameters: {
+        LayerName: 'lambda-layer-kubectl'
+      }
+    })
 
-        new cdk.CfnOutput(this, 'SamFuncArn', {
-          value: eksAuthHook.getAtt('Outputs.LambdaFuncArn').toString(),
-        })
 
-        new cdk.CfnOutput(this, 'NodeInstanceRole', {
-          value: asg.role.roleArn,
-        })
+    // cfn custom resource to handle aws-auth-cm update
+    const eksAuthHook = new sam.CfnApplication(this, 'sam', {
+      location: {
+        applicationId: 'arn:aws:serverlessrepo:us-east-1:903779448426:applications/eks-auth-update-hook',
+        semanticVersion: '1.0.0',
+      },
+      parameters: {
+        ClusterName: 'eksdemo-cdk',
+        // LambdaRoleArn: 'arn:aws:iam::903779448426:role/AmazonEKSAdminRole',
+        LambdaRoleArn: lambdaRoleArn,
+        LambdaLayerKubectlArn: lambdaLayerKubectl.getAtt('Outputs.LayerVersionArn').toString(),
+        NodeInstanceRoleArn: asg.role.roleArn,
+        FunctionName: 'eks-auth-hook'
+      }
+    })
 
-    }
+    const customResourceAuthHook = new cfn.CustomResource(this, 'CustomResource', {
+      provider: cfn.CustomResourceProvider.lambda(lambda.Function.fromFunctionArn(this, 'SamFunc', eksAuthHook.ref)),
+      properties: {
+        ServiceToken: eksAuthHook.getAtt('Outputs.LambdaFuncArn').toString()
+      }
+    });
+
+    customResourceAuthHook.node.addDependency(eksCluster)
+
+    new cdk.CfnOutput(this, 'SamFuncArn', {
+      value: eksAuthHook.getAtt('Outputs.LambdaFuncArn').toString(),
+    })
+
+    new cdk.CfnOutput(this, 'NodeInstanceRole', {
+      value: asg.role.roleArn,
+    })
+
+  }
 }
 
 // create the VPC stack now
